93de541776dc0c9c89ea7c160914d8d18f8a84d8
MarcoCali0 <marcocali@protonmail.com>
2023-10-29 11:16:40 +0100

    refactoring

--- exercise2_ws/src/exercise2/src/charging_station.cpp
+++ exercise2_ws/src/exercise2/src/charging_station.cpp
@@ -1,62 +1,67 @@
-#include "ros/ros.h"
 #include "exercise2/Service.h"
+#include "ros/ros.h"
 #include "std_msgs/Header.h"
 
-class ChargingStation
-{
-public:
-    ChargingStation(int station_ID, int timer)
-    {
-        this->seq = 0;
-        this->station_ID = station_ID;
-        this->timer = timer;
+// Define a class for the ChargingStation
+class ChargingStation {
+ public:
+  ChargingStation(int station_ID, int timer) {
+    this->seq = 0;                  // Initialize sequence number
+    this->station_ID = station_ID;  // Initialize station_ID
+    this->timer = timer;            // Initialize timer
 
-        // generate the header for the service request
-        this->srv.request.header = std_msgs::Header();
+    // Generate the header for the service request
+    this->srv.request.header = std_msgs::Header();
 
-        // generate the station ID for the service request
-        this->srv.request.station_ID = this->station_ID;
+    // Generate the station ID for the service request
+    this->srv.request.station_ID = this->station_ID;
 
-        // create node for service request
-        this->client = n.serviceClient<exercise2::Service>("/get_robot_state");
+    // Create a ROS service client for the "/get_robot_state" service
+    this->client = n.serviceClient<exercise2::Service>("/get_robot_state");
+  }
+
+  // Function to request robot state from a service
+  void ask_robot_state(const ros::TimerEvent &event) {
+    // Update header for the service request w/ current time and sequence number
+    this->srv.request.header.stamp = ros::Time::now();
+    this->srv.request.header.seq = this->seq;
+    this->seq++;  // Increment the sequence number
+
+    if (client.call(this->srv)) {
+      ROS_INFO(
+          "Charging Station %d, Current Battery Level: %d %%. Room %s (ID %d)",
+          this->station_ID, this->srv.response.message.charge_level,
+          this->srv.response.message.room_name.c_str(),
+          this->srv.response.message.room_ID);
     }
+  }
 
-    void ask_robot_state(const ros::TimerEvent &event)
-    {
-        // update the header for the service request
-        this->srv.request.header.stamp = ros::Time::now();
-        this->srv.request.header.seq = this->seq;
-        this->seq++;
-
-        if (client.call(this->srv))
-        {
-            // TODO: must print request and response (not very important)
-            ROS_INFO("Current Battery Level: %d %%. Room %s (ID %d)",
-                     this->srv.response.message.charge_level, this->srv.response.message.room_name.c_str(), this->srv.response.message.room_ID);
-        }
-    }
-
-private:
-    int seq;
-    int station_ID;
-    int timer;
-    exercise2::Service srv;
-    ros::NodeHandle n;
-    ros::ServiceClient client;
+ private:
+  int seq;                    // Sequence number for service requests
+  int station_ID;             // ID of the charging station
+  int timer;                  // Timer duration for periodic requests
+  exercise2::Service srv;     // Service message for the request
+  ros::NodeHandle n;          // Node handle for the ChargingStation class
+  ros::ServiceClient client;  // Service client to call the  service
 };
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "charging_station");
-    ros::NodeHandle n;
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "charging_station");
+  ros::NodeHandle n;
 
-    // Fetch parameters from launch file
-    int station_ID, timer;
-    n.getParam("station_ID", station_ID);
-    n.getParam("timer", timer);
+  // Fetch parameters from the launch file (station_ID and timer)
+  int station_ID, timer;
+  n.getParam("station_ID", station_ID);
+  n.getParam("timer", timer);
 
-    ChargingStation charging_station(station_ID, timer);
-    ros::Timer t = n.createTimer(ros::Duration(timer), &ChargingStation::ask_robot_state, &charging_station);
-    ros::spin();
-    return 0;
+  // Create a ChargingStation instance with the provided parameters
+  ChargingStation charging_station(station_ID, timer);
+
+  // Create a timer that periodically calls the ask_robot_state service
+  ros::Timer t =
+      n.createTimer(ros::Duration(timer), &ChargingStation::ask_robot_state,
+                    &charging_station);
+
+  ros::spin();
+  return 0;
 }
--- exercise2_ws/src/exercise2/src/cleaner_robot.cpp
+++ exercise2_ws/src/exercise2/src/cleaner_robot.cpp
@@ -1,86 +1,95 @@
-#include "ros/ros.h"
 #include "exercise2/Message.h"
-#include "std_msgs/Header.h"
 #include "exercise2/Service.h"
+#include "ros/ros.h"
+#include "std_msgs/Header.h"
 
-struct room
-{
-    std::string room_name;
-    int8_t ID;
+// Define a structure for room information
+struct room {
+  std::string room_name;
+  int8_t ID;
 };
 
-room rooms[5] = {
-    {"Robot Vision Lab", 1},
-    {"SSL Lab", 2},
-    {"Neurorobotics Lab", 3},
-    {"IAS Lab", 4},
-    {"Autonomous Robotics Lab", 5}};
+// Initialize an array of rooms
+room rooms[5] = {{"Robot Vision Lab", 1},
+                 {"SSL Lab", 2},
+                 {"Neurorobotics Lab", 3},
+                 {"IAS Lab", 4},
+                 {"Autonomous Robotics Lab", 5}};
 
-class Robot
-{
-public:
-    Robot(uint8_t initial_battery, room initial_room)
-    {
-        this->battery = initial_battery;
-        this->current_room = initial_room;
-        this->seq = 0;
-    }
+// Define a class for the Robot
+class Robot {
+ public:
+  Robot(uint8_t initial_battery, room initial_room) {
+    this->battery = initial_battery;    // Initialize battery level
+    this->current_room = initial_room;  // Initialize current room
+    this->seq = 0;                      // Initialize sequence number
+  }
 
-    bool get_robot_state(exercise2::Service::Request &req, exercise2::Service::Response &res)
-    {
-        // Notice we're not using the request data, however this could be use for discriminating
-        // between the node making the requests and sending different data
+  // Service callback to get the robot's state
+  bool get_robot_state(exercise2::Service::Request &req,
+                       exercise2::Service::Response &res) {
+    // Notice that the request data is not used, but it could be used to
+    // discriminate between nodes and send different data.
 
-        // generate the header of the service response
-        res.header = std_msgs::Header();
-        res.header.stamp = ros::Time::now();
-        res.header.seq = this->seq;
-        this->seq++;
+    // Generate the header of the service response
+    res.header = std_msgs::Header();
+    res.header.stamp = ros::Time::now();
+    res.header.seq = this->seq;
+    this->seq++;  // Increment the sequence number
 
-        // generate the message of the service response
-        res.message = exercise2::Message();
-        res.message.room_name = this->current_room.room_name;
-        res.message.room_ID = this->current_room.ID;
-        res.message.charge_level = this->battery;
+    // Generate the message of the service response
+    res.message = exercise2::Message();
+    res.message.room_name = this->current_room.room_name;
+    res.message.room_ID = this->current_room.ID;
+    res.message.charge_level = this->battery;
 
-        return true;
-    }
+    return true;
+  }
 
-    void update_battery(const ros::TimerEvent &event)
-    {
-        // Reset battery when it's discharged
-        this->battery = (this->battery > 0) ? (this->battery - 1) : 100;
-    }
+  // Timer callback to update the battery level
+  void update_battery(const ros::TimerEvent &event) {
+    // Reset the battery when it's discharged
+    this->battery = (this->battery > 0) ? (this->battery - 1) : 100;
+  }
 
-    void update_room(const ros::TimerEvent &event)
-    {
-        int index_current_room = this->current_room.ID - 1;
-        this->current_room = (index_current_room == 4) ? this->current_room = rooms[0] : rooms[index_current_room + 1];
-    }
+  // Timer callback to update the current room
+  void update_room(const ros::TimerEvent &event) {
+    int index_current_room = this->current_room.ID - 1;
+    this->current_room = (index_current_room == 4)
+                             ? this->current_room = rooms[0]
+                             : rooms[index_current_room + 1];
+  }
 
-private:
-    uint8_t battery;
-    room current_room;
-    int seq;
+ private:
+  uint8_t battery;    // Battery level of the robot
+  room current_room;  // Current room the robot is in
+  int seq;            // Sequence number for service responses
 };
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "cleaner_robot");
-    ros::NodeHandle n;
-    ROS_INFO("Cleaner Robot Node Started");
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "cleaner_robot");
+  ros::NodeHandle n;
 
-    Robot robot(100, rooms[0]);
+  ROS_INFO("Cleaner Robot Node Started");
 
-    int battery_life_seconds = 1000;
-    // The battery decreases by 1% every 10 seconds
-    ros::Timer update_battery = n.createTimer(ros::Duration(battery_life_seconds / 100), &Robot::update_battery, &robot);
+  // Create a Robot instance with an initial battery level and room
+  Robot robot(100, rooms[0]);
 
-    int duration_cleaning = 20;
-    // The robot cleans the same room for 20 seconds
-    ros::Timer update_room = n.createTimer(ros::Duration(duration_cleaning), &Robot::update_room, &robot);
+  // The battery decreases by 1% every 10 seconds
+  int battery_life_seconds = 1000;
+  ros::Timer update_battery =
+      n.createTimer(ros::Duration(battery_life_seconds / 100),
+                    &Robot::update_battery, &robot);
 
-    ros::ServiceServer service = n.advertiseService("/get_robot_state", &Robot::get_robot_state, &robot);
-    ros::spin();
-    return 0;
+  // The robot cleans the same room for 20 seconds
+  int duration_cleaning = 20;
+  ros::Timer update_room = n.createTimer(ros::Duration(duration_cleaning),
+                                         &Robot::update_room, &robot);
+
+  // Create a service server to handle requests for robot state
+  ros::ServiceServer service =
+      n.advertiseService("/get_robot_state", &Robot::get_robot_state, &robot);
+
+  ros::spin();
+  return 0;
 }
